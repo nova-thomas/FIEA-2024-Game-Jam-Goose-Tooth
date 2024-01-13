eddd52938468f473a55a9884570706023806c265
Nova Thomas <novathomas2002@gmail.com>
2024-01-13 00:46:54 -0500

    Applied solar wind physics inverted to black holes

--- FIEA Game Jam 2024 Project/Assets/Scripts/BlackHoleBehavior.cs
+++ FIEA Game Jam 2024 Project/Assets/Scripts/BlackHoleBehavior.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BlackHoleBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float force = 1;
+    public Rigidbody rb;
+    public GameObject ship;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        BlackHoleSuction();
+    }
+
+    // Applying Solar Wind Force
+    void BlackHoleSuction()
+    {
+        Vector3 direction = rb.position - ship.transform.position;
+        float distance = direction.magnitude;
+
+        float forceMagnitude = (rb.mass * ship.GetComponent<Rigidbody>().mass) / Mathf.Pow(distance, 2);
+        Vector3 solarForce = direction.normalized * forceMagnitude;
+        ship.GetComponent<Rigidbody>().AddForce(solarForce);
     }
 }
