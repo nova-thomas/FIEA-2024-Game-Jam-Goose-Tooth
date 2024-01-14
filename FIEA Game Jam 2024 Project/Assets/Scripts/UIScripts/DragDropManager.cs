5a394c9f07a3670ded8d17c93d2ccf18b9d4d96f
Nova Thomas <novathomas2002@gmail.com>
2024-01-14 02:01:13 -0500

    Fixed bug in instantiation of objects

--- FIEA Game Jam 2024 Project/Assets/Scripts/UIScripts/DragDropManager.cs
+++ FIEA Game Jam 2024 Project/Assets/Scripts/UIScripts/DragDropManager.cs
@@ -7,14 +7,35 @@ public class DragDropManager : MonoBehaviour
     public List<DragDrop> draggedSprites = new List<DragDrop>();
     public List<GameObject> instantiatedObjects = new List<GameObject>();
 
-    public void InstantiatePrefabAtPointOnAll(Vector3 pos)
+    public Vector3 GetSpawnPosition(RectTransform rectTransform)
+    {
+        // Use ray to find the position based on the individual dragged sprite
+        Ray ray = Camera.main.ScreenPointToRay(rectTransform.position);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("PlayField"))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * 200f, Color.red);
+            float fixedY = 0f;
+            Vector3 spawnPos = new Vector3(hit.point.x, fixedY, hit.point.z);
+            return spawnPos;
+        }
+        Debug.Log("Spawn Position: Not Hit");
+        // If raycast doesn't hit the playfield, return Vector3.zero or handle accordingly
+        return Vector3.zero;
+    }
+    public void InstantiatePrefabAtPointOnAll()
     {
         foreach (DragDrop scriptInstance in draggedSprites)
         {
             if (scriptInstance != null)
             {
-                GameObject instantiatedObject = Instantiate(scriptInstance.prefabToInstantiate, pos, Quaternion.identity);
-                instantiatedObjects.Add(instantiatedObject);
+                Vector3 spawnPosition = GetSpawnPosition(scriptInstance.rectTransform);
+                if (spawnPosition != Vector3.zero)
+                {
+                    GameObject instantiatedObject = Instantiate(scriptInstance.prefabToInstantiate, spawnPosition, Quaternion.identity);
+                    instantiatedObjects.Add(instantiatedObject);
+                }
             }
         }
     }
--- FIEA Game Jam 2024 Project/Assets/Scripts/UIScripts/OnPlay.cs
+++ FIEA Game Jam 2024 Project/Assets/Scripts/UIScripts/OnPlay.cs
@@ -13,31 +13,12 @@ public class OnPlay : MonoBehaviour
             dragDropManager = playObject.GetComponent<DragDropManager>();
         }
     }
-    public Vector3 GetSpawnPosition(RectTransform rectTransform)
-    {
-        // Use ray to find the position based on the individual dragged sprite
-        Ray ray = Camera.main.ScreenPointToRay(rectTransform.position);
-        RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit) && hit.collider.CompareTag("PlayField"))
-        {
-            Debug.DrawRay(ray.origin, ray.direction * 200f, Color.red);
-            float fixedY = 0f;
-            Vector3 spawnPos = new Vector3(hit.point.x, fixedY, hit.point.z);
-            return spawnPos;
-        }
-        Debug.Log("Spawn Position: Not Hit");
-        // If raycast doesn't hit the playfield, return Vector3.zero or handle accordingly
-        return Vector3.zero;
-    }
     public void onPlayClick()
     {
         // Adding prefabs to playfield
-        foreach (DragDrop draggedSprite in dragDropManager.draggedSprites)
-        {
-            Vector3 spawnPosition = GetSpawnPosition(draggedSprite.rectTransform);
-            dragDropManager.InstantiatePrefabAtPointOnAll(spawnPosition);
-        }
+        dragDropManager.InstantiatePrefabAtPointOnAll();
+
         // Remove sprites from UI
         // Hide UI Sprites
         foreach (DragDrop draggedSprite in dragDropManager.draggedSprites)
